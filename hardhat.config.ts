7f2620edb1bcaff957a7f71c643b16081ff8c0a3
Nick Barry <itsnickbarry@protonmail.ch>
2023-08-21 17:53:04 -0400

    disable optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,12 +7,6 @@ import { HardhatUserConfig } from 'hardhat/types';
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.21',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
   },
 
   spdxLicenseIdentifier: {
